1535dda79ef72a2f61c418ac3242ec6419d25b01
huthmazo <44196469+huthmazo@users.noreply.github.com>
2018-10-17 08:42:36 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,12 +1,8 @@
 <?php
 
-//echo '<pre>' . var_export($_FILES, true) . '</pre>';
-//echo '<pre>' . var_export($_POST, true) . '</pre>';
-
 require('clases/upload.php');
+
 $archivo = new Upload($_FILES['file']);
-//$archivo->setPolicy(Upload::POLICY_RENAME);
-//$r = $archivo->upload();
+
 $archivo->upload();
-//echo '<pre>' . var_export($r, true) . '</pre>';
 
